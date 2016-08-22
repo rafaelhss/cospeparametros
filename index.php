629afae7ab180b28e4c6e8f64daf3e519f156d4d
rafael <rafael.soares@bcb.gov.br>
2016-08-22 15:31:35 -0300

    versao teste

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo '<h3>Taaps - Teste formularios HTML</h3><h1>Parametros Recebidos:</h1>:';
+echo '<h3>Taaps - Teste formularios HTML</h3><h1>Parametros Recebidos:</h1>';
 foreach($_GET as $key => $value){
   echo "<h3>" . $key . " : " . $value . "</h3>";
 }
